53fab85aff156bcc131905c28eac8c8f8a381ff3
Dzmitry Alkhimovich <dm.alkhimovich@gmail.com>
2016-12-20 20:39:51 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,21 +1,20 @@
 var strPrompted = prompt('Введите произвольный текст');
 
-var brOpen = 0;
-var brClosed = 0;
+var br = 0;
 
 for (var i = 0; i < strPrompted.length; i++) {
   if (strPrompted[i] == '(' ) {
-    brOpen += 1;
+    br++;
   }
   if (strPrompted[i] == ')' ) {
-    brClosed += 1;
+    br--;
   }
 }
 
-  if (brOpen > brClosed) {
-  alert('Пропущено закрывающихся скобок: ' + (brOpen - brClosed) );
-} else if (brClosed > brOpen) {
-  alert('Пропущено открывающихся скобок: ' + (brClosed - brOpen) );
+  if (br > 0) {
+  alert('Пропущено закрывающихся скобок: ' + br);
+} else if (br < 0) {
+  alert('Пропущено открывающихся скобок: ' + Math.abs(br) );
 } else {
   alert('OK')
 }
